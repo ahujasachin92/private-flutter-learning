34fbf952321e2249c5c20d5a08e7cc25ae36125e
ahujasachin92 <ahujasachin9229@gmail.com>
2022-12-25 19:00:29 +0530

    Flutter Learning - Expense App

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+//import 'package:flutter_expense_app/widgets/user_transactions.dart';
+
+class NewTransaction extends StatefulWidget {
+  final Function addTx;
+
+  NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate = DateTime.now(); 
+
+  void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
+    
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
+      return;
+      //print ('hello!');
+    }
+    widget.addTx(
+      enteredTitle, 
+      enteredAmount,
+      _selectedDate,
+      );
+    Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context, 
+      initialDate: DateTime.now(), 
+      firstDate: DateTime(2019), 
+      lastDate: DateTime.now(),
+      ).then((pickedDate) {
+        if (pickedDate == null) 
+        {
+        return;
+        }
+        setState(() {
+          _selectedDate = pickedDate;
+          });
+        });
+      print('...');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+            elevation: 5.0,
+            child: 
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children:<Widget>[
+                TextField(
+                  decoration: InputDecoration(labelText: 'Title'),
+                  controller: _titleController,
+                  onSubmitted: (_) => _submitData(),
+                  //onChanged: (val) {
+                  //  titleInput = val;
+                  //},
+                  ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Amount'),
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  onSubmitted: (_) => _submitData(),
+                  //onChanged: (val) => amountInput = val,
+                ),
+                Container(
+                  height: 70,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          _selectedDate == null 
+                          ? 'No chosen date!'
+                          : DateFormat.yMd().format(_selectedDate),
+                          ),
+                      ),
+                      TextButton(
+                        onPressed: _presentDatePicker, 
+                        child: Text(
+                          'Choose Date',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        style: ButtonStyle(
+                          foregroundColor: MaterialStateProperty.all(Colors.purple),
+                        ),
+                        ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  child: Text('Add transaction'), 
+                  //style: ElevatedButton.styleFrom(
+                    //foregroundColor: Colors.white,
+                    //backgroundColor: Colors.purple,
+                  //),
+                  onPressed: _submitData,
+                  //() { 
+                    //addTx(
+                      //titleController.text, 
+                      //double.parse(amountController.text),
+                ),
+                    //print(titleController.text);
+                    //print(amountController.text);
+                  //),
+              ],
+            ),
+          ),
+          );
+  }
+}
